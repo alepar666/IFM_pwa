11c1a1107c3b7a15c0f6b2d432c4c7022fc69893
Alessandro Parisi <alessandro.parisi@gmail.com>
2026-03-17 19:23:52 +0100

    next version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // increment at every new deploy
-const CACHE_NAME = `ifm-cache-1773771291248`;
+const CACHE_NAME = `ifm-cache-1773771773532`;
 
 // Base path dinamico
 const BASE_PATH = self.location.pathname.replace(/\/[^\/]*$/, '/');
