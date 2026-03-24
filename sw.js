73701ba7db842af67175ce196dc2ebbc871a2c6e
Alessandro Parisi <alessandro.parisi@gmail.com>
2026-03-24 12:20:32 +0100

    added timestamp to js and css imports during build to test if this force the mobile cache to be renewed

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // increment at every new deploy
-const CACHE_NAME = `ifm-cache-1774348903198`;
+const CACHE_NAME = `ifm-cache-1774351144828`;
 
 // Base path dinamico
 const BASE_PATH = self.location.pathname.replace(/\/[^\/]*$/, '/');
