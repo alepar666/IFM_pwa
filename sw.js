6ef44c221d5ef64fa52d541d8418bdd67f18f66e
Alessandro Parisi <alessandro.parisi@gmail.com>
2026-04-14 12:22:32 +0200

    updated version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = `ifm-cache-1776161846853`;
+const CACHE_NAME = `ifm-cache-1776162131136`;
 const BASE_PATH = self.location.pathname.replace(/\/[^\/]*$/, '/');
 
 const PRECACHE_ASSETS = [
