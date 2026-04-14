d05046c0915430d8740475190db4cb216731a3d4
Alessandro Parisi <alessandro.parisi@gmail.com>
2026-04-14 15:54:11 +0200

    fixed cover view for android

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = `ifm-cache-1776173980632`;
+const CACHE_NAME = `ifm-cache-1776174834303`;
 const BASE_PATH = self.location.pathname.replace(/\/[^\/]*$/, '/');
 
 const PRECACHE_ASSETS = [
